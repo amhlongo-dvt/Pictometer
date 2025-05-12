af3199a2cef1dab9aa17b51986e75897eb3b8d3f
Andile Mhlongo <majiyaz2@gmail.com>
2025-05-12 13:19:55 +0200

    Added validation for chats

--- src/controllers/chat.ts
+++ src/controllers/chat.ts
@@ -7,9 +7,24 @@ import type {
     DBCreateMessage,
  } from "../models/db";
 import type { IDatabaseResource } from "../storage/types";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+
+const idSchema = z.object({
+    id: z.string().min(1),
+})
+
+const chatSchema = z.object({
+    name: z.string().min(1),
+})
+
+const messageSchema = z.object({
+    message: z.string().min(1),
+})
 
 export const CHAT_PREFIX = "/chat/";
 export const CHAT_ROUTE = "";
+export const CHAT_DETAIL_ROUTE = ":id";
 export const CHAT_MESSAGE_ROUTE = ":id/message/";
 
 export function createChatApp(
@@ -18,9 +33,9 @@ export function createChatApp(
 ) {
     const chatApp = new Hono<ContextVariables>();
 
-    chatApp.post(CHAT_ROUTE, async (c) => {
+    chatApp.post(CHAT_ROUTE, zValidator("json", chatSchema), async (c) => {
         const userId = c.get("userId");
-        const {name} = await c.req.json()
+        const {name} = c.req.valid("json")
         const chat = await chatResource.create({name, ownerId: userId})
         return c.json({chat})
     })
@@ -30,16 +45,23 @@ export function createChatApp(
         const chats = await chatResource.findAll({ownerId: userId})
         return c.json({chats})
     })
+    
+    chatApp.get(CHAT_DETAIL_ROUTE, zValidator("param", idSchema), async (c) => {
+        const {id} = c.req.valid("param")
+        const userId = c.get("userId");
+        const data = await chatResource.find({id, ownerId: userId})
+        return c.json({data})
+    })
 
-    chatApp.get(CHAT_MESSAGE_ROUTE, async (c) => {
-        const {id: chatId} = c.req.param()
+    chatApp.get(CHAT_MESSAGE_ROUTE, zValidator("param", idSchema), async (c) => {
+        const {id: chatId} = c.req.valid("param")
         const chats = await messageResource.findAll({chatId})
         return c.json({chats})
     })
 
-    chatApp.post(CHAT_MESSAGE_ROUTE, async (c) => {
-        const {id: chatId} = c.req.param()
-        const {message} = await c.req.json()
+    chatApp.post(CHAT_MESSAGE_ROUTE, zValidator("param", idSchema), zValidator("json", messageSchema), async (c) => {
+        const {id: chatId} = c.req.valid("param")
+        const {message} = c.req.valid("json")
 
         const userMessage: DBCreateMessage = {message, chatId, type:"user"}
 
