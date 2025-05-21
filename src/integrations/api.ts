e80c5111b5d537cffccaed05c1a1c04f3f78b53d
Andile Mhlongo <majiyaz2@gmail.com>
2025-05-21 18:29:00 +0200

    feat: added the main api call for completions

---
+++ src/integrations/api.ts
@@ -0,0 +1,11 @@
+export async function callAPI(data:object ){
+    const res = await fetch("https://api.mistral.ai/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${Bun.env.MISTRAL_API_KEY}`
+        },
+        body: JSON.stringify(data)
+    })
+    return res
+}
