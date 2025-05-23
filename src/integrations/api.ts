52863992c242d1c5c89f0cc5e0bcc01b464825ca
Andile Mhlongo <majiyaz2@gmail.com>
2025-05-23 13:31:15 +0200

    feat: compibined the gpt api and util for getting a completion

--- src/integrations/api.ts
+++ src/integrations/api.ts
@@ -1,4 +1,4 @@
-export async function callAPI(data:object ){
+export async function callGPTAPI(data:object ){
     const res = await fetch("https://api.mistral.ai/v1/chat/completions", {
         method: "POST",
         headers: {
