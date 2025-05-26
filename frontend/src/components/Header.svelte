6a3c1cecc3fbef29aa1e15e7ed8bd18a3b46bf6c
Andile Mhlongo <majiyaz2@gmail.com>
2025-05-26 12:26:55 +0200

    feat: added a header component

---
+++ frontend/src/components/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import {navigate} from "svelte-routing"
+    import {authToken} from '../stores/auth'
+    const name = authToken.getPayload()?.name || "User"
+    
+    function logout() {
+        authToken.remove()
+        navigate("/login")
+    }
+</script>
+
+<div class="header">
+    <div class="user-name">{name}</div>
+    <button class="logout-button" on:click={logout}>
+        Logout
+    </button>
+</div>
