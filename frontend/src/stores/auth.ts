52c0e699467c78f9768c779200c786eee679e7c6
Andile Mhlongo <majiyaz2@gmail.com>
2025-05-26 10:05:12 +0200

    feat: defining the authentication store

---
+++ frontend/src/stores/auth.ts
@@ -0,0 +1,48 @@
+import { writable } from "svelte/store";
+import axios from "axios";
+import {jwtDecode} from "jwt-decode";
+
+interface TokenPayLoad{
+    name: string
+}
+
+function setAxiosAuth(token: string) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
+}
+
+function createAuthStore() {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+        setAxiosAuth(token)
+    }
+
+    const {subscribe, set} = writable<string | null>(token)
+
+    return{
+        subscribe,
+        set: (value: string) => {
+            localStorage.setItem("authToken", value);
+            set(value)
+            if (value){
+                setAxiosAuth(value)
+            }else{
+                delete axios.defaults.headers.common["Authorization"];
+            }
+        },
+        remove: () => {
+            localStorage.removeItem("authToken");
+            set(null);
+            delete axios.defaults.headers.common["Authorization"];
+        },
+        getPayload: () => {
+            const token = localStorage.getItem("authToken");
+            if(token) {
+                const decoded: TokenPayLoad = jwtDecode(token)
+                return decoded
+            }   
+            return null
+        },
+    };
+}
+
+export const authToken = createAuthStore()
