36d35fd173142d5ce79517d72ada4e66ecd668b2
Andile Mhlongo <majiyaz2@gmail.com>
2025-06-03 19:26:45 +0200

    feat: added new components(tabs, image card)

---
+++ frontend/src/lib/components/ui/image-card/image-card.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { HTMLAttributes } from "svelte/elements";
+	import { cn } from "$lib/utils.js";
+
+	interface $$Props extends HTMLAttributes<HTMLElement> {
+		imageUrl: string;
+		caption: string;
+		class?: string;
+	}
+
+	export let imageUrl: string;
+	export let caption: string;
+	
+	let className: $$Props["class"] = undefined;
+	export { className as class };
+</script>
+
+<figure
+	class={cn(
+		"w-full h-full overflow-hidden rounded-base border-2 border-border bg-main font-base shadow-shadow flex flex-col",
+		className
+	)}
+	{...$$restProps}
+>
+	<img 
+		class="w-full flex-1 min-h-0 " 
+		src={imageUrl} 
+		alt={caption} 
+	/>
+	<figcaption class="border-t-2 text-mtext border-border p-4 flex-shrink-0">
+		{caption}
+	</figcaption>
+</figure>
